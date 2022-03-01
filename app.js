250b35fb08c12d4bec8a29d2aebbe910fc71409c
naimxno <naimxno@gmail.com>
2022-03-01 23:38:42 +0600

    complete without revesion

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// phones result load
 document.getElementById('button-addon2').addEventListener('click', () => {
   searchBox = document.getElementById('search-box');
   searchText = searchBox.value;
@@ -7,13 +8,12 @@ document.getElementById('button-addon2').addEventListener('click', () => {
     .then(res => res.json())
     .then(data => displaySearchResult(data.data));
 })
-
+// display phone result
 const displaySearchResult = mobiles => {
   const searchresult = document.getElementById("search-result");
   searchresult.innerHTML = '';
   mobiles = mobiles.slice(0, 20);
   if (mobiles.length > 0) {
-
     mobiles.forEach(mobile => {
       const div = document.createElement('div');
       div.classList.add('col');
@@ -34,21 +34,21 @@ const displaySearchResult = mobiles => {
     searchresult.innerHTML = `<h1 class="w-100 text-center">No Phone Found</h1>`
   }
 }
-
+// single phone data load
 const showDetails = phoneId => {
   const idUrl = `https://openapi.programming-hero.com/api/phone/${phoneId}`
   fetch(idUrl)
     .then(res => res.json())
     .then(data => detailsResult(data.data));
 }
-
+// single phone details
 const detailsResult = details => {
   console.log(details);
   const section = document.getElementById("showFullDetails");
   section.innerHTML = `
       <div class="card shadow-sm my-3 mx-auto text-start  container-sm">
           <img src="${details.image}" class="card-img-top w-auto m-auto p-2" alt="...">
-          <div class="card-body">
+          <div class="card-body text-center">
             <h5 class="card-title">${details.brand}</h5>
             <h5 class="card-text">${details.name}</h5>
             <p>${details.releaseDate ? details.releaseDate : "no release found"}</p>
